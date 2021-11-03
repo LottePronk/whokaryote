1858cfe2b87f5345ce4c80b0d81703edd2cc9476
lotte.pronk <lotte.pronk@wur.nl>
2021-11-03 15:13:12 +0100

    minor changes

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="contig_classifier",  # Replace with your own username
+    name="Whokaryote",  # Replace with your own username
     version="0.0.1",
     author="Lotte Pronk",
     author_email="lotte.pronk@wur.nl",
-    description="A fast and easy tool to predict if a metagenomic contig is eukaryotic or prokaryotic",
+    description="Whokaryote: A fast and easy tool to predict if a metagenomic contig is eukaryotic or prokaryotic",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/",
