180b6daa8cf2c66fe16d03b44d08926ebf6db6eb
lotte.pronk <lotte.pronk@wur.nl>
2021-11-04 11:21:32 +0100

    minor changes

--- setup.py
+++ setup.py
@@ -4,24 +4,22 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Whokaryote",  # Replace with your own username
+    name="Whokaryote",
     version="0.0.1",
     author="Lotte Pronk",
     author_email="lotte.pronk@wur.nl",
     description="Whokaryote: A fast and easy tool to predict if a metagenomic contig is eukaryotic or prokaryotic",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.com/",
+    url="https://git.wageningenur.nl/lotte.pronk/whokaryote",
     project_urls={
-        "Bug Tracker": "https://gitlab.com",
+        "Bug Tracker": "https://git.wageningenur.nl/lotte.pronk/whokaryote",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: GNU Affero General Public License v3.0",
         "Operating System :: Linux or MacOS",
     ],
-    #  package_dir={"": "src"},
-    #  packages=setuptools.find_packages(where="src"),
     packages=['whokaryote_scripts'],
     include_package_data=True,
     python_requires=">=3.8",
