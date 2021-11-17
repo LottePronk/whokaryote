a44e2cdd0e2fce7740aa1a25c7a9e189fee0961c
lotte.pronk <lotte.pronk@wur.nl>
2021-11-17 14:26:53 +0100

    Tiara will now work with a minimum contig length of 1000 bp

--- whokaryote_scripts/run_tiara.py
+++ whokaryote_scripts/run_tiara.py
@@ -8,4 +8,4 @@ def run_tiara(contig_file, outdir):
     prediction_output = os.path.join(outdir, "tiara_pred.txt")
 
     subprocess.call(
-        ["tiara", "-i", contig_file, "-o", prediction_output, "-m 3000"])
+        ["tiara", "-i", contig_file, "-o", prediction_output, "-m 1000"])
